d68f4bfb4a4377583230e2b5a9d5bb7bcddf620f
emilynvs <emilynascimento22@outlook.com>
2025-11-28 23:38:12 -0300

    feat: adiciona listar notas

--- arquivo.py
+++ arquivo.py
@@ -2,16 +2,20 @@ import redis
 
 rds = redis.Redis(host='localhost', port=6379, db=0)
 
-
 def adicionar_nota(texto):
     ids = rds.keys("nota:*")
-    novo_id = len(ids) + 1
+    novo_id = rds.incr('contador')
 
     chave_nota = f"nota:{novo_id}"
 
     rds.set(chave_nota, texto)
 
-adicionar_nota("Comprar melancia")
+def listar_tudo():
+    ids = rds.keys("nota:*")
+    for id in ids:
+        id_atual = rds.get(id)
+        print(id.decode(), id_atual.decode())
+
 
 
 
