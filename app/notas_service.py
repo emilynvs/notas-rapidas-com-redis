0888a57e2ed92e749781eedbb52dcc0871f89d9e
emilynvs <emilynascimento22@outlook.com>
2025-11-30 10:14:57 -0300

    refactor: ajuste de variável e remoção de chamadas desnecessárias

--- app/notas_service.py
+++ app/notas_service.py
@@ -1,23 +1,23 @@
 from app.conexao import rds
 
 def adicionar_nota(texto):
-    ids = rds.keys("nota:*")
     novo_id = rds.incr('contador')
     chave_nota = f"nota:{novo_id}"
     rds.set(chave_nota, texto)
 
+
 def listar_tudo():
     ids = rds.keys("nota:*")
     for id in ids:
-        id_atual = rds.get(id)
-        print(id, id_atual)
+        valor = rds.get(id)
+        print(id, valor)
 
 def remover_nota():
     listar_tudo()
     remover_id = int(input("Digite o número da nota que deseja remover: "))
     rds.delete(f"nota:{remover_id}")
     print("Nota deletada")
-    listar_tudo()
+
 
 def atualizar_nota():
     listar_tudo()
@@ -26,7 +26,7 @@ def atualizar_nota():
     rds.set(f"nota:{id_nota}", novo_texto)
 
     print("Nota atualizada")
-    listar_tudo()
+
 
 def buscar_nota():
     ...
